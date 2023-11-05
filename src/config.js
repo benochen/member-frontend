3026b715c88ab8e9f58b767f8f09d2d97263f713
benoit <ben57univ@gmail.com>
2023-11-05 14:57:57 +0100

    Correct config object

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config={
     openid_connect:{
-        "redirct_uri":"https://membres.chenal.int:63342/mockup-memebrs/membres.html",
+        "redirct_uri":"https://membres.chenal.int:8000/members",
         "url_prefix":"https://accounts.google.com/o/oauth2/v2/auth?",
         "clientid":"697261480006-6onc6cb2jq2un380ein2oh1p4alqqi8s.apps.googleusercontent.com",
         "scope":"openid%20email",
@@ -8,7 +8,7 @@ let config={
         "issuer":'https://accounts.google.com'
 },
     api:{
-        "api_url":"https://membres.chenal.int:8000/membership/members",
+        "api_url":"https://api.membres.chenal.int:9000/membership/members",
     }
 }
 
