5c4dac0e0540f1e3015e2f0f5c09dfbab8973079
benoit <ben57univ@gmail.com>
2023-11-06 23:20:36 +0100

    Remove TLS check

--- src/services_membres.js
+++ src/services_membres.js
@@ -1,10 +1,16 @@
 import config from "./config";
+
 const api_url=config.api.api_url
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+
 
 export  async function get_all_members(){
 
     try{
         let access_token=sessionStorage.getItem("ACCESS8TOKEN")
+        const agent = new https.Agent({
+            rejectUnauthorized: false
+        })
         let response= await fetch(api_url,{
             method:'GET',
             headers: {Authorization: 'Bearer '+ access_token}
