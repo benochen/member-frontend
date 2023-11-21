f4f608827f79fadaaeaf7cf848c93f12d1e4bc0b
benoit <ben57univ@gmail.com>
2023-11-21 22:51:49 +0100

    Remove spin

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config={
     openid_connect:{
-        "redirct_uri":"https://membres.chenal.int:8000/members",
+        "redirct_uri":"https://membres.chenal.int:8000/",
         "url_prefix":"https://accounts.google.com/o/oauth2/v2/auth?",
         "clientid":"697261480006-6onc6cb2jq2un380ein2oh1p4alqqi8s.apps.googleusercontent.com",
         "scope":"openid%20email",
